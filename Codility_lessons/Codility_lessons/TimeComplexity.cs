3c65d1beec07eac2d31dfa22361f3e9cfe5e5ce0
ioneltuc <ion.tentiuc@isa.utm.md>
2023-10-05 18:15:59 +0300

    PermMissingElem test resolved

--- Codility_lessons/Codility_lessons/TimeComplexity.cs
+++ Codility_lessons/Codility_lessons/TimeComplexity.cs
@@ -7,4 +7,27 @@ public class TimeComplexity
         double distance = y - x;
         return (int)Math.Ceiling(distance / d);
     }
+
+    public static int PermMissingElem(int[] array)
+    {
+        if (array.Length == 0)
+            return 1;
+
+        if (array.Length == 1 && array.First() == 1)
+            return array.First() + 1;
+
+        if (array.Length == 1 && array.First() != 1)
+            return array.First() - 1; 
+
+        Array.Sort(array);
+
+        if (array[0] != 1)
+            return 1;
+        
+        var missingElem = Enumerable
+            .Range(array.First(), array.Length)
+            .Except(array.ToList());
+
+        return missingElem.Count() == 0 ? array.Max() + 1 : missingElem.First();
+    }
 }
