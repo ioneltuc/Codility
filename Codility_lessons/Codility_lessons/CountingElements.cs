9450d83c6c02aa2e63689263a4694cfd2b45a8c6
ioneltuc <ion.tentiuc@isa.utm.md>
2023-10-06 11:20:29 +0300

    FrogRiverOne resolved, but bad performance

--- Codility_lessons/Codility_lessons/CountingElements.cs
+++ Codility_lessons/Codility_lessons/CountingElements.cs
@@ -11,9 +11,9 @@ public class CountingElements
 
             return 0;
         }
-        
+
         int[] occurrences = new int[array.Max() + 1];
-        
+
         for (int i = 0; i < array.Length; i++)
             occurrences[array[i]]++;
 
@@ -24,5 +24,45 @@ public class CountingElements
         }
 
         return 1;
+    }
+
+    public static int MissingInteger(int[] array)
+    {
+        if (array.Any(x => x < 0))
+            return 1;
+
+        int[] arrayNoDuplicates = array.Distinct().ToArray();
+
+        int[] range = Enumerable.Range(0, arrayNoDuplicates.Length + 1).ToArray();
+
+        int diff = range.Sum() - arrayNoDuplicates.Sum();
+
+        return diff == 0 ? array.Max() + 1 : diff;
+    }
+
+    public static int FrogRiverOne(int x, int[] fallingLeaves)
+    {
+        bool isLastLeave = false;
+        
+        for (int i = 0; i < fallingLeaves.Length; i++)
+        {
+            int[] potentialResult = new int[i + 1];
+            Array.Copy(fallingLeaves, potentialResult, i + 1);
+
+            int[] occurences = new int[potentialResult.Max()];
+
+            for (int j = 0; j < potentialResult.Length; j++)
+            {
+                if (potentialResult[j] == x)
+                    isLastLeave = true;
+
+                occurences[potentialResult[j] - 1]++;
+            }
+
+            if (!occurences.Contains(0) && isLastLeave)
+                return i;
+        }
+
+        return -1;
     }
 }
--- Codility_lessons/Codility_lessons/Program.cs
+++ Codility_lessons/Codility_lessons/Program.cs
@@ -1,3 +1,3 @@
 ﻿using Codility_lessons;
 
-CountingElements.PermCheck(new int[] {7});
+Console.Write(CountingElements.FrogRiverOne(5,new int[] {1,3,1,4,1,3,5,4,5,1,1,2,5}));
