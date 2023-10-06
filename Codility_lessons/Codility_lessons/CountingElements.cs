ea985f563aec409cfefc61714c1affb65feca0ff
ioneltuc <ion.tentiuc@isa.utm.md>
2023-10-06 12:08:28 +0300

    Missing integer changes

--- Codility_lessons/Codility_lessons/CountingElements.cs
+++ Codility_lessons/Codility_lessons/CountingElements.cs
@@ -28,16 +28,26 @@ public class CountingElements
 
     public static int MissingInteger(int[] array)
     {
-        if (array.Any(x => x < 0))
+        if (array.Any(x => x < 1))
             return 1;
 
+        if (array.Length == 1)
+        {
+            if (array[0] != 1)
+                return array[0] - 1;
+        }
+        
         int[] arrayNoDuplicates = array.Distinct().ToArray();
 
-        int[] range = Enumerable.Range(0, arrayNoDuplicates.Length + 1).ToArray();
+        int[] range = Enumerable
+            .Range(1, arrayNoDuplicates.Max() - arrayNoDuplicates.Min() + 1)
+            .ToArray();
+
+        int[] missingElements = range.Except(arrayNoDuplicates).ToArray();
 
         int diff = range.Sum() - arrayNoDuplicates.Sum();
 
-        return diff == 0 ? array.Max() + 1 : diff;
+        return diff == 0 ? array.Max() + 1 : missingElements.Min();
     }
 
     public static int FrogRiverOne(int x, int[] fallingLeaves)
--- Codility_lessons/Codility_lessons/Program.cs
+++ Codility_lessons/Codility_lessons/Program.cs
@@ -1,3 +1,3 @@
 ﻿using Codility_lessons;
 
-Console.Write(CountingElements.FrogRiverOne(5,new int[] {1,3,1,4,1,3,5,4,5,1,1,2,5}));
+CountingElements.MissingInteger(new int[] {1});
