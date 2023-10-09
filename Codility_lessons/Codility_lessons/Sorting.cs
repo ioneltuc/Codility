a90248632ae6b1331610be6662f9cf4dba2b27ab
ioneltuc <ion.tentiuc@isa.utm.md>
2023-10-09 09:32:34 +0300

    MaxProductOfThree test resolved

--- Codility_lessons/Codility_lessons/Sorting.cs
+++ Codility_lessons/Codility_lessons/Sorting.cs
@@ -6,4 +6,14 @@ public class Sorting
     {
         return array.Distinct().Count();
     }
+
+    public static int MaxProductOfThree(int[] array)
+    {
+        Array.Sort(array);
+
+        if (array[^1] * array[^2] * array[^3] > array[0] * array[1] * array[^1])
+            return array[^1] * array[^2] * array[^3];
+
+        return array[0] * array[1] * array[^1];
+    }
 }
