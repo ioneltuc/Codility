31f70227a88776e7822c180f0afd778256b30e79
ioneltuc <ion.tentiuc@isa.utm.md>
2023-10-09 09:38:08 +0300

    Triangle test resolved

--- Codility_lessons/Codility_lessons/Sorting.cs
+++ Codility_lessons/Codility_lessons/Sorting.cs
@@ -16,4 +16,26 @@ public class Sorting
 
         return array[0] * array[1] * array[^1];
     }
+
+    public static int Triangle(int[] array)
+    {
+        if (array.Length < 3)
+            return 0;
+
+        Array.Sort(array);
+
+        //daca tabelul e sortat, automat array[i + 2] + array[i + 1] > array[i]
+        //si array[i + 2] + array[i] > array[i + 1]
+        //ramane sa verificam daca array[i] + array[i + 1] > array[i + 2]
+        for (int i = 0; i < array.Length - 2; i++)
+        {
+            //echivalent cu array[i] + array[i + 1] > array[i + 2]
+            //trecem array[i + i] in dreapta pentru a
+            //trece testul cu toate 3 MaxInteger
+            if (array[i] > array[i + 2] - array[i + 1])
+                return 1;
+        }
+
+        return 0;
+    }
 }
