db4b8d5644c43450bc497de8844f30c33583ccfb
ioneltuc <ion.tentiuc@isa.utm.md>
2023-10-09 09:03:51 +0300

    MinAvgTwoSlice test resolved

--- Codility_lessons/Codility_lessons/PrefixSums.cs
+++ Codility_lessons/Codility_lessons/PrefixSums.cs
@@ -58,21 +58,52 @@ public class PrefixSums
 
             switch (nucleotide)
             {
-                case 'A' :
+                case 'A':
                     result[i] = 1;
                     break;
-                case 'C' :
+                case 'C':
                     result[i] = 2;
                     break;
-                case 'G' :
+                case 'G':
                     result[i] = 3;
                     break;
-                case 'T' :
+                case 'T':
                     result[i] = 4;
                     break;
             }
         }
 
         return result;
+    }
+
+    public static int MinAvgTwoSlice(int[] array)
+    {
+        double minAvg = double.MaxValue;
+        int startingPos = 0;
+
+        for (int i = 0; i < array.Length - 2; i++)
+        {
+            int sumOfTwo = array[i] + array[i + 1];
+            int sumOfThree = array[i] + array[i + 1] + array[i + 2];
+
+            if (sumOfThree / 3.0 < minAvg)
+            {
+                minAvg = sumOfThree / 3.0;
+                startingPos = i;
+            }
+
+            if (sumOfTwo / 2.0 < minAvg)
+            {
+                minAvg = sumOfTwo / 2.0;
+                startingPos = i;
+            }
+        }
+
+        if ((array[^1] + array[^2]) / 2.0 < minAvg)
+        {
+            startingPos = array.Length - 2;
+        }
+
+        return startingPos;
     }
 }
--- Codility_lessons/Codility_lessons/Program.cs
+++ Codility_lessons/Codility_lessons/Program.cs
@@ -1,3 +1,4 @@
 ﻿using Codility_lessons;
 
-PrefixSums.GenomicRangeQuery("CAGCCTA", new int[] {2,5,0}, new int[] {4,5,6});
+// PrefixSums.GenomicRangeQuery("CAGCCTA", new int[] {2,5,0}, new int[] {4,5,6});
+PrefixSums.MinAvgTwoSlice(new int[] {4, 2, 2, 5, 1, 5, 8});
