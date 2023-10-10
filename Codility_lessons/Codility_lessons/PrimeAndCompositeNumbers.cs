0173b8aae9b8412116eb4527e2e35263d2af5029
ioneltuc <ion.tentiuc@isa.utm.md>
2023-10-10 13:43:12 +0300

    resolved MinPerimeterRectangle test

--- Codility_lessons/Codility_lessons/PrimeAndCompositeNumbers.cs
+++ Codility_lessons/Codility_lessons/PrimeAndCompositeNumbers.cs
@@ -17,7 +17,32 @@ public class PrimeAndCompositeNumbers
 
         if (i * i == n)
             factorsCount++;
-        
+
         return factorsCount;
+    }
+
+    public static int MinPerimeterRectangle(int n)
+    {
+        int minPerimeter = int.MaxValue;
+        int perimeter = 0;
+        int i = 1;
+
+        while (i * i <= n)
+        {
+            if (n % i == 0)
+                perimeter = CalculatePerimeter(i, n / i);
+
+            if (perimeter < minPerimeter)
+                minPerimeter = perimeter;
+
+            i++;
+        }
+
+        return minPerimeter;
+    }
+
+    private static int CalculatePerimeter(int a, int b)
+    {
+        return 2 * (a + b);
     }
 }
--- Codility_lessons/Codility_lessons/Program.cs
+++ Codility_lessons/Codility_lessons/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Codility_lessons;
+
+PrimeAndCompositeNumbers.MinPerimeterRectangle(36);
