eca5d33d1db2b8dc4b165fe307c290ac8c6be6f2
ioneltuc <ion.tentiuc@isa.utm.md>
2023-10-11 14:05:18 +0300

    resolved CountNonDivisible

--- Codility_lessons/Codility_lessons/Program.cs
+++ Codility_lessons/Codility_lessons/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Codility_lessons;
 
 var sieveTasks = new SieveOfEratosthenes();
-sieveTasks.CountSemiprimes(26, new int[]{1,4,16}, new int[] {26,10,20});
+// sieveTasks.CountSemiprimes(26, new int[]{1,4,16}, new int[] {26,10,20});
+SieveOfEratosthenes.CountNonDivisible(new int[] {3,1,2,3,6});
--- Codility_lessons/Codility_lessons/SieveOfEratosthenes.cs
+++ Codility_lessons/Codility_lessons/SieveOfEratosthenes.cs
@@ -4,65 +4,58 @@ public class SieveOfEratosthenes
 {
     public static int[] CountNonDivisible(int[] a)
     {
-        int[] result = new int[a.Length];
+        // int[] result = new int[a.Length];
+        //
+        // for (int i = 0; i < a.Length; i++)
+        // {
+        //     int nonDivisorsCount = 0;
+        //
+        //     for (int j = 0; j < a.Length; j++)
+        //     {
+        //         if (i == j)
+        //             continue;
+        //         
+        //         if (a[i] % a[j] != 0)
+        //         {
+        //             nonDivisorsCount++;
+        //         }
+        //     }
+        //
+        //     result[i] = nonDivisorsCount;
+        // }
+        //
+        // return result;
 
+        int[] occurrences = new int[a.Max() + 1];
+        
         for (int i = 0; i < a.Length; i++)
         {
-            int nonDivisorsCount = 0;
+            occurrences[a[i]]++;
+        }
 
-            for (int j = 0; j < a.Length; j++)
+        int[] result = new int[a.Length];
+        
+        for (int i = 0; i < a.Length; i++)
+        {
+            int divisorsCount = 0;
+            int k = 1;
+            
+            while (k * k <= a[i])
             {
-                if (i == j)
-                    continue;
-                
-                if (a[i] % a[j] != 0)
+                if (a[i] % k == 0)
                 {
-                    nonDivisorsCount++;
+                    divisorsCount += occurrences[k];
+
+                    if (a[i] / k != k)
+                        divisorsCount += occurrences[a[i] / k];
                 }
+
+                k++;
             }
 
-            result[i] = nonDivisorsCount;
+            result[i] = a.Length - divisorsCount;
         }
 
-        return result;
-    }
-
-    public int[] CountSemiprimes(int n, int[] p, int[] q)
-    {
-        int[] result = new int[p.Length];
-        PrimeNumbersFiller.Fill(n / 2);
-        int[] primeNumbers = PrimeNumbersFiller.PrimeNumbers.ToArray();
-        List<int> semiprimes = new List<int>();
-        int productOfTwoPrimes = 0;
-        
-        for (int i = 0; i < primeNumbers.Length; i++)
-        {
-            for (int j = 0; j < primeNumbers.Length; j++)
-            {
-                productOfTwoPrimes = primeNumbers[i] * primeNumbers[j];
-
-                if (productOfTwoPrimes > n)
-                    break;
-                
-                semiprimes.Add(productOfTwoPrimes);
-            }
-        }
-
-        semiprimes = semiprimes.Distinct().Order().ToList();
-
-        for (int i = 0; i < p.Length; i++)
-        {
-            int from = semiprimes.First(x => x >= p[i]);
-            int to = semiprimes.Last(x => x <= q[i]);
-            int fromIndex = semiprimes.IndexOf(from);
-            int toIndex = semiprimes.IndexOf(to);
-
-            if (fromIndex > toIndex)
-                result[i] = 0;
-            else
-                result[i] = semiprimes.Skip(fromIndex).Take(toIndex + 1).Count();
-        }
-        
         return result;
     }
 }
